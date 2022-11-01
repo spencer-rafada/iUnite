9846717cb385b45df586bee4b87ac2f1089f42ed
Spencer Rafada <spencer.byui@gmail.com>
2022-10-31 20:05:47 -0600

    Initial Commit

---
+++ iUnite/iUniteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iUniteApp.swift
+//  iUnite
+//
+//  Created by Spencer Rafada on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct iUniteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
